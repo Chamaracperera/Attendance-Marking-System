bbc8fadc0bf44d61b0d9e3a33e92a11c4edb087c
Chamara perera <wchamaraask@gmail.com>
2025-02-10 18:20:45 +0530

    Add files via upload

---
+++ mailer.php
@@ -0,0 +1,26 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+$mail->Username   = 'qrbasedattendancesystem@gmail.com';                     //SMTP username
+$mail->Password   = 'ahigccnhoxvhvhws';
+
+$mail->isHtml(true);
+
+return $mail;
